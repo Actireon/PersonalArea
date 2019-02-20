ae24a11f389e59d9bb72f9c26d4c7a4cde6bcaf3
Actireon <eheieiei37@gmail.com>
2019-02-20 18:16:11 +0200

    modal-optimization[2]

--- js/modal.js
+++ js/modal.js
@@ -3,34 +3,28 @@ document.querySelectorAll('.modal-show').forEach(function (element) {
 });
 
 document.querySelectorAll('.modal-close').forEach(function (element) {
+    //closing modal on button
     element.onclick = closeModal;
 });
 
 document.querySelectorAll('.modal-wrap').forEach(function (element) {
-    //console.log(element);
-    element.onclick = closeModalWrap;
+    //closing modal on gray area
+    element.onclick = closeModal;
 });
 
 function showModal() {
     let modalId = this.dataset.modal;
     document.querySelector(modalId).classList.remove('hide');
     document.onkeydown = function (event) {
-        if (event.keyCode == 27) {
-            document.querySelectorAll('.modal-wrap').forEach(function (element) {
-                element.classList.add('hide');
-                document.onkeydown = null;
-            });
-        }
+        //closing modal on ESC
+        if (event.keyCode == 27) closeModal();
     }
 }
 
 function closeModal() {
-    let modalId = this.dataset.modal;
-    document.querySelector(modalId).classList.add('hide');
+    document.querySelectorAll('.modal-wrap').forEach(function (element) {
+        element.classList.add('hide');
+    });
     document.onkeydown = null;
 }
 
-function closeModalWrap() {
-    this.classList.add('hide');
-    document.onkeydown = null;
-}
